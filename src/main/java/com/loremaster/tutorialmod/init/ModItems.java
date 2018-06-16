212ab9eb2055af6db4e669b7396b455175b4129f
Thomas Peters <thomasanthonypeters@gmail.com>
2018-06-15 18:11:35 -0700

    Add custom armor

--- src/main/java/com/loremaster/tutorialmod/init/ModItems.java
+++ src/main/java/com/loremaster/tutorialmod/init/ModItems.java
@@ -4,14 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.loremaster.tutorialmod.items.ItemBase;
+import com.loremaster.tutorialmod.items.armor.ArmorBase;
 import com.loremaster.tutorialmod.items.tools.ToolAxe;
 import com.loremaster.tutorialmod.items.tools.ToolHoe;
 import com.loremaster.tutorialmod.items.tools.ToolPickaxe;
 import com.loremaster.tutorialmod.items.tools.ToolSpade;
 import com.loremaster.tutorialmod.items.tools.ToolSword;
+import com.loremaster.tutorialmod.util.Reference;
 
+import net.minecraft.init.SoundEvents;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemArmor.ArmorMaterial;
 import net.minecraft.item.ItemAxe;
 import net.minecraft.item.ItemHoe;
 import net.minecraft.item.ItemPickaxe;
@@ -25,7 +30,9 @@ public class ModItems
 	
 	//Materials
 	public static final ToolMaterial MATERIAL_RUBY = EnumHelper.addToolMaterial("material_ruby", 3, 250, 8.0F, 3.0F, 10);
-	
+	public static final ArmorMaterial ARMOR_MATERIAL_RUBY = EnumHelper.addArmorMaterial("armor_material_ruby", Reference.MOD_ID + ":ruby", 14, 
+			new int[] {2, 5, 7, 3}, 10, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 0.0F);
+
 	//Items
 	public static final Item RUBY = new ItemBase("ruby");
 	public static final Item OBSIDIAN_INGOT = new ItemBase("obsidian_ingot");
@@ -36,5 +43,11 @@ public class ModItems
 	public static final ItemPickaxe RUBY_PICKAXE = new ToolPickaxe("ruby_pickaxe", MATERIAL_RUBY);
 	public static final ItemAxe RUBY_AXE = new ToolAxe("ruby_axe", MATERIAL_RUBY);
 	public static final ItemHoe RUBY_HOE = new ToolHoe("ruby_hoe", MATERIAL_RUBY);
-
+	
+	//Armor
+	public static final Item RUBY_HELMET = new ArmorBase("ruby_helmet", ARMOR_MATERIAL_RUBY, 1, EntityEquipmentSlot.HEAD);
+	public static final Item RUBY_CHESTPLATE = new ArmorBase("ruby_chestplate", ARMOR_MATERIAL_RUBY, 1, EntityEquipmentSlot.CHEST);
+	public static final Item RUBY_LEGGINGS = new ArmorBase("ruby_leggings", ARMOR_MATERIAL_RUBY, 2, EntityEquipmentSlot.LEGS);
+	public static final Item RUBY_BOOTS = new ArmorBase("ruby_boots", ARMOR_MATERIAL_RUBY, 1, EntityEquipmentSlot.FEET);
+	
 }
---
+++ src/main/java/com/loremaster/tutorialmod/items/armor/ArmorBase.java
@@ -0,0 +1,28 @@
+package com.loremaster.tutorialmod.items.armor;
+
+import com.loremaster.tutorialmod.Main;
+import com.loremaster.tutorialmod.init.ModItems;
+import com.loremaster.tutorialmod.util.IHasModel;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemArmor;
+
+public class ArmorBase extends ItemArmor implements IHasModel {
+
+	public ArmorBase(String name, ArmorMaterial materialIn, int renderIndexIn, EntityEquipmentSlot equipmentSlotIn) {
+		super(materialIn, renderIndexIn, equipmentSlotIn);
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setCreativeTab(CreativeTabs.COMBAT);
+		
+		ModItems.ITEMS.add(this);
+	}
+	
+	@Override
+	public void registerModels() 
+	{
+		Main.proxy.registerItemRenderer(this, 0, "inventory");
+	}
+
+}
